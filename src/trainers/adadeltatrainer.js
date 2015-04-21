9580a4ccec3894a8f4bacdcab83c2982f8a5dd42
Thomas Foster <thomasjamesfoster@bigpond.com>
2015-04-21 17:42:38 +1000

    SIMDify AdadeltaTrainer

--- src/trainers/adadeltatrainer.js
+++ src/trainers/adadeltatrainer.js
@@ -11,26 +11,31 @@ export default class AdadeltaTrainer extends Trainer {
 
 	train(x, y){
 
-		var start_1 = new Date().getTime();
+		let start_1 = new Date().getTime();
     this.net.forward(x, true); // also set the flag that lets the net know we're just training
-    var fwd_time = (new Date().getTime()) - start;
+    let fwd_time = (new Date().getTime()) - start;
 
-    var start_2 = new Date().getTime();
-    var cost_loss = this.net.backward(y);
-    var bwd_time = (new Date().getTime()) - start;
+    let start_2 = new Date().getTime();
+    let cost_loss = this.net.backward(y);
+    let bwd_time = (new Date().getTime()) - start;
 
-    var l2_decay_loss = 0.0;
-    var l1_decay_loss = 0.0; 
+    let l2_decay_loss = SIMD.float32x4.splat(0.0);
+    let l1_decay_loss = SIMD.float32x4.splat(0.0);
+
+    let ro = SIMD.float32x4.splat(this.ro);
+    let rom = SIMD.float32x4.splat(1-this.ro);
+    let eps = SIMD.float32x4.splat(this.eps);
       
     this.k++;
+
     if(this.k % this.batch_size === 0) {
 
-      var pglist = this.net.getParamsAndGrads();
+      let pglist = this.net.getParamsAndGrads();
 
       // initialize lists for accumulators. Will only be done once on first iteration
       if(this.gsum.length === 0) {
         // adadelta needs gsum and xsum
-        for(var i = 0; i < pglist.length; i++) {
+        for(let i = 0; i < pglist.length; i++) {
           this.gsum.push(new Float64Array(pglist[i].params.length));
           this.xsum.push(new Float64Array(pglist[i].params.length));
         }
@@ -39,33 +44,50 @@ export default class AdadeltaTrainer extends Trainer {
       let pglen = (pglist.length|0);
 
       // perform an update for all sets of weights
-      for(var i = 0; i < pglen; i++) {
-        let {p, g, ...pg} = pglist[i]; // param, gradient, other options in future (custom learning rate etc)
+      for(let i = 0; i < pglen; i++) {
+        let {p, g, l2_decay_mul, l1_decay_mul} = pglist[i]; // param, gradient, other options in future (custom learning rate etc)
 
         // learning rate for some parameters.
-        let l2_decay = this.l2_decay * (pg.l2_decay_mul || 1.0);
-        let l1_decay = this.l1_decay * (pg.l1_decay_mul || 1.0);
+        let l2_decay = SIMD.float32x4.splat(this.l2_decay * (l2_decay_mul || 1.0));
+        let l1_decay = SIMD.float32x4.splat(this.l1_decay * (l1_decay_mul || 1.0));
 
-        let plen = (p.length|0);
+        let gsumi = this.gsum[i];
+        let xsumi = this.xsum[i];
 
-        for(var j = 0; j < plen; j++) {
+        let plen = (p.length/4|0); 
 
-          l2_decay_loss += +(l2_decay*p[j]*p[j]/2); // accumulate weight decay loss
-          l1_decay_loss += +(l1_decay*Math.abs(p[j]));
-          let l1grad = l1_decay * (p[j] > 0 ? 1 : -1);
-          let l2grad = l2_decay * (p[j]);
+        /*
+         * SIMD spaghetti code ahead - best served warm and w/ bolognaise sauce.
+         */
 
-          let gij = (l2grad + l1grad + g[j]) / this.batch_size; // raw batch gradient
+        for(let j = 0; j < plen; j++) {
 
-          let gsumi = this.gsum[i];
-          let xsumi = this.xsum[i];
+          let pj = SIMD.float32x4(p[j], p[j+1], p[j+2], p[j+3]);
+          let gj = SIMD.float32x4(g[j], g[j+1], g[j+2], g[j+3]);
+          let gsumij = SIMD.float32x4(gsumi[j], gsumi[j+1], gsumi[j+2], gsumi[j+3]);
+          let xsumij = SIMD.float32x4(xsumi[j], xsumi[j+1], xsumi[j+2], xsumi[j+3]);
+
+          // accumulate weight decay loss
+          l2_decay_loss = SIMD.float32x4.add(l2_decay_loss, SIMD.float32x4.div(SIMD.float32x4.mul(l1_decay, SIMD.float32x4.mul(pj, pj)), SIMD.float32x4.splat(2)));
+          l1_decay_loss = SIMD.float32x4.add(l1_decay_loss, SIMD.mul(l1_decay, SIMD.float32x4.abs(pj)));
+
+          let l1grad = SIMD.float32x4.mul(l1_decay, SIMD.float32x4.greaterThan(pj, SIMD.float32x4.splat(0.0)));
+          let l2grad = SIMD.float32x4.mul(l2_decay, pj)
+
+          let gij = SIMD.float32x4.add(l2grad, SIMD.float32x4.add(l1grad, gj)) // raw batch gradient
 		    
-          gsumi[j] = this.ro * gsumi[j] + (1-this.ro) * gij * gij;
-          let dx = - Math.sqrt((xsumi[j] + this.eps)/(gsumi[j] + this.eps)) * gij;
-          xsumi[j] = this.ro * xsumi[j] + (1-this.ro) * dx * dx; // yes, xsum lags behind gsum by 1.
-          p[j] += dx;
+          gsumij = SIMD.float32x4.add(SIMD.float32x4.mul(ro, gsumij), SIMD.float32x4.mul(rom, SIMD.float32x4.mul(gij, gij)));
+          let dx = SIMD.float32x4.neg(SIMD.float32x4.mul(SIMD.float32x4.sqrt(SIMD.float32x4.div(SIMD.float32x4.add(xsumij, eps), SIMD.float32x4.add(gsumij, eps))), gij));
+          xsumij = SIMD.float32x4.add(SIMD.float32x4.mul(ro, gsumij), SIMD.float32x4.mul(rom, SIMD.float32x4.mul(dx, dx)));
+          // yes, xsum lags behind gsum by 1.
+
+          gsumi[j] = gsumij.x; gsumi[j+1] = gsumij.y; gsumi[j+2] = gsumij.z; gsumi[j+3] = gsumij.w;
+          xsumi[j] = xsumij.x; xsumi[j+1] = xsumij.y; xsumi[j+2] = xsumij.z; xsumi[j+3] = xsumij.w;
+
+          p[j] += dx.x; p[j+1] += dx.y; p[j+2] += dx.z; p[j+3] += dx.w;
           
-          g[j] = 0.0; // zero out gradient so that we can begin accumulating anew
+          // zero out gradient so that we can begin accumulating anew
+          g[j] = 0.0; g[j+1] = 0.0; g[j+2] = 0.0; g[j+3] = 0.0;
         }
       }
     }
@@ -76,13 +98,13 @@ export default class AdadeltaTrainer extends Trainer {
     // and it should all be computed correctly and automatically. 
     return {
       fwd_time: fwd_time, 
-      bwd_time: bwd_time, 
-      l2_decay_loss: l2_decay_loss, 
-      l1_decay_loss: l1_decay_loss,
+      bwd_time: bwd_time,
+      l2_decay_loss: (l2_decay_loss.x, l2_decay_loss.y, l2_decay_loss.z, l2_decay_loss.w), 
+      l1_decay_loss: (l1_decay_loss.x, l1_decay_loss.y, l1_decay_loss.z, l1_decay_loss.w),
       cost_loss: cost_loss, 
       softmax_loss: cost_loss, 
-      loss: cost_loss + l1_decay_loss + l2_decay_loss
-    };
+      loss: cost_loss + (l1_decay_loss.x, l1_decay_loss.y, l1_decay_loss.z, l1_decay_loss.w) + (l2_decay_loss.x, l2_decay_loss.y, l2_decay_loss.z, l2_decay_loss.w)
+    }
 
 	}
 
