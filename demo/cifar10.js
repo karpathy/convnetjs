292de185dcd385922aae53226f07adc5d6bec10a
Pavel Pepeldjiyski <pdp@sfu.ca>
2015-05-07 00:40:32 -0700

    refactored cifar10 and mnist javascript files

--- demo/cifar10.js
+++ demo/cifar10.js
@@ -1,3 +1,16 @@
+// ------------------------
+// BEGIN CIFAR-10 SPECIFIC STUFF
+// ------------------------
+var classes_txt = ['airplane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'];
+var dataset_name = "cifar10";
+var num_batches = 51; // 20 training batches, 1 test
+var test_batch = 50;
+var num_samples_per_batch = 1000;
+var image_dimension = 32;
+var image_channels = 3;
+var use_validation_data = true;
+var random_flip = true;
+var random_position = true;
 
 var layer_defs, net, trainer;
 var t = "layer_defs = [];\n\
@@ -15,23 +28,19 @@ net.makeLayers(layer_defs);\n\
 \n\
 trainer = new convnetjs.SGDTrainer(net, {method:'adadelta', batch_size:4, l2_decay:0.0001});\n\
 ";
-
 // ------------------------
-// BEGIN MNIST SPECIFIC STUFF
+// END CIFAR-10 SPECIFIC STUFF
 // ------------------------
-var classes_txt = ['airplane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'];
 
-var use_validation_data = true;
 var sample_training_instance = function() {
-
   // find an unloaded batch
   var bi = Math.floor(Math.random()*loaded_train_batches.length);
   var b = loaded_train_batches[bi];
-  var k = Math.floor(Math.random()*1000); // sample within the batch
-  var n = b*1000+k;
+  var k = Math.floor(Math.random()*num_samples_per_batch); // sample within the batch
+  var n = b*num_samples_per_batch+k;
 
   // load more batches over time
-  if(step_num%2000===0 && step_num>0) {
+  if(step_num%(2 * num_samples_per_batch)===0 && step_num>0) {
     for(var i=0;i<num_batches;i++) {
       if(!loaded[i]) {
         // load it
@@ -43,22 +52,29 @@ var sample_training_instance = function() {
 
   // fetch the appropriate row of the training image and reshape into a Vol
   var p = img_data[b].data;
-  var x = new convnetjs.Vol(32,32,3,0.0);
-  var W = 32*32;
+  var x = new convnetjs.Vol(image_dimension,image_dimension,image_channels,0.0);
+  var W = image_dimension*image_dimension;
   var j=0;
   for(var dc=0;dc<3;dc++) {
     var i=0;
-    for(var xc=0;xc<32;xc++) {
-      for(var yc=0;yc<32;yc++) {
+    for(var xc=0;xc<image_dimension;xc++) {
+      for(var yc=0;yc<image_dimension;yc++) {
         var ix = ((W * k) + i) * 4 + dc;
         x.set(yc,xc,dc,p[ix]/255.0-0.5);
         i++;
       }
     }
   }
-  var dx = Math.floor(Math.random()*5-2);
-  var dy = Math.floor(Math.random()*5-2);
-  x = convnetjs.augment(x, 32, dx, dy, Math.random()<0.5); //maybe flip horizontally
+
+  if(random_position){
+    var dx = Math.floor(Math.random()*5-2);
+    var dy = Math.floor(Math.random()*5-2);
+    x = convnetjs.augment(x, image_dimension, dx, dy, false); //maybe change position
+  }
+
+  if(random_flip){
+    x = convnetjs.augment(x, image_dimension, 0, 0, Math.random()<0.5); //maybe flip horizontally
+  }
 
   var isval = use_validation_data && n%10===0 ? true : false;
   return {x:x, label:labels[n], isval:isval};
@@ -68,17 +84,17 @@ var sample_training_instance = function() {
 var sample_test_instance = function() {
 
   var b = test_batch;
-  var k = Math.floor(Math.random()*1000);
-  var n = b*1000+k;
+  var k = Math.floor(Math.random()*num_samples_per_batch);
+  var n = b*num_samples_per_batch+k;
 
   var p = img_data[b].data;
-  var x = new convnetjs.Vol(32,32,3,0.0);
-  var W = 32*32;
+  var x = new convnetjs.Vol(image_dimension,image_dimension,image_channels,0.0);
+  var W = image_dimension*image_dimension;
   var j=0;
   for(var dc=0;dc<3;dc++) {
     var i=0;
-    for(var xc=0;xc<32;xc++) {
-      for(var yc=0;yc<32;yc++) {
+    for(var xc=0;xc<image_dimension;xc++) {
+      for(var yc=0;yc<image_dimension;yc++) {
         var ix = ((W * k) + i) * 4 + dc;
         x.set(yc,xc,dc,p[ix]/255.0-0.5);
         i++;
@@ -88,11 +104,24 @@ var sample_test_instance = function() {
 
   // distort position and maybe flip
   var xs = [];
-  //xs.push(x, 32, 0, 0, false); // push an un-augmented copy
-  for(var k=0;k<6;k++) {
-    var dx = Math.floor(Math.random()*5-2);
-    var dy = Math.floor(Math.random()*5-2);
-    xs.push(convnetjs.augment(x, 32, dx, dy, k>2));
+  
+  if (random_flip || random_position){
+    for(var k=0;k<6;k++) {
+      var test_variation = x;
+      if(random_position){
+        var dx = Math.floor(Math.random()*5-2);
+        var dy = Math.floor(Math.random()*5-2);
+        test_variation = convnetjs.augment(test_variation, image_dimension, dx, dy, false);
+      }
+      
+      if(random_flip){
+        test_variation = convnetjs.augment(test_variation, image_dimension, 0, 0, Math.random()<0.5); 
+      }
+
+      xs.push(test_variation);
+    }
+  }else{
+    xs.push(x, image_dimension, 0, 0, false); // push an un-augmented copy
   }
   
   // return multiple augmentations, and we will average the network over them
@@ -100,8 +129,7 @@ var sample_test_instance = function() {
   return {x:xs, label:labels[n]};
 }
 
-var num_batches = 51; // 20 training batches, 1 test
-var test_batch = 50;
+
 var data_img_elts = new Array(num_batches);
 var img_data = new Array(num_batches);
 var loaded = new Array(num_batches);
@@ -112,13 +140,13 @@ $(window).load(function() {
 
   $("#newnet").val(t);
   eval($("#newnet").val());
-  
+
   update_net_param_display();
 
   for(var k=0;k<loaded.length;k++) { loaded[k] = false; }
 
-  load_data_batch(0); // async load train set batch 0 (6 total train batches)
-  load_data_batch(test_batch); // async load test set (batch 6)
+  load_data_batch(0); // async load train set batch 0
+  load_data_batch(test_batch); // async load test set
   start_fun();
 });
 
@@ -145,12 +173,8 @@ var load_data_batch = function(batch_num) {
     if(batch_num < test_batch) { loaded_train_batches.push(batch_num); }
     console.log('finished loading data batch ' + batch_num);
   };
-  data_img_elt.src = "cifar10/cifar10_batch_" + batch_num + ".png";
+  data_img_elt.src = dataset_name + "/" + dataset_name + "_batch_" + batch_num + ".png";
 }
-
-// ------------------------
-// END MNIST SPECIFIC STUFF
-// ------------------------
 
 var maxmin = cnnutil.maxmin;
 var f2t = cnnutil.f2t;
@@ -466,6 +490,7 @@ var test_predict = function() {
   $("#testset_acc").text('test accuracy based on last 200 test images: ' + testAccWindow.get_average());  
 }
 
+
 var lossGraph = new cnnvis.Graph();
 var xLossWindow = new cnnutil.Window(100);
 var wLossWindow = new cnnutil.Window(100);
@@ -605,7 +630,7 @@ var load_from_json = function() {
 }
 
 var load_pretrained = function() {
-  $.getJSON("cifar10_snapshot.json", function(json){
+  $.getJSON(dataset_name + "_snapshot.json", function(json){
     net = new convnetjs.Net();
     net.fromJSON(json);
     trainer.learning_rate = 0.0001;
