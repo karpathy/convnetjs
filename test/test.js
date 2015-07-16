3a41d705a3ac14ce9596ddc723d2822f68c2b013
Thomas Foster <thomasjamesfoster@bigpond.com>
2015-07-16 23:44:00 +1000

    try using coveralls

--- test/test.js
+++ test/test.js
@@ -21,6 +21,21 @@ describe('Structures', function(){
 			var vol = new voltype();
 			assert('dw' in vol);
 		});
+		it('should have a field called sx', function(){
+			var voltype = new convnet.VolType();
+			var vol = new voltype();
+			assert('sx' in vol);
+		});
+		it('should have a field called sy', function(){
+			var voltype = new convnet.VolType();
+			var vol = new voltype();
+			assert('sy' in vol);
+		});
+		it('should have a field called depth', function(){
+			var voltype = new convnet.VolType();
+			var vol = new voltype();
+			assert('depth' in vol);
+		});
 	});
 	describe('Experience', function(){
 		it('should work', function(){
@@ -60,6 +75,41 @@ describe('Layers', function(){
 			new convnet.MaxoutLayer({group_size: 10, in_sx: 1, in_sy: 1, in_depth: 5});
 		});
 	});
+	describe('PoolLayer', function(){
+		it('should work', function(){
+			new convnet.PoolLayer({sx: 5, in_sx: 1, in_sy: 1, in_depth: 10});
+		});
+	});
+	describe('RegressionLayer', function(){
+		it('should work', function(){
+			new convnet.RegressionLayer({in_sx: 1, in_sy: 1, in_depth: 5});
+		});
+	});
+	describe('ReluLayer', function(){
+		it('should work', function(){
+			new convnet.ReluLayer({in_sx: 1, in_sy: 1, in_depth: 5});
+		});
+	});
+	describe('SigmoidLayer', function(){
+		it('should work', function(){
+			new convnet.SigmoidLayer({in_sx: 1, in_sy: 1, in_depth: 5});
+		});
+	});
+	describe('SoftmaxLayer', function(){
+		it('should work', function(){
+			new convnet.SoftmaxLayer({in_sx: 1, in_sy: 1, in_depth: 5});
+		});
+	});
+	describe('SVMLayer', function(){
+		it('should work', function(){
+			new convnet.SVMLayer({in_sx: 1, in_sy: 1, in_depth: 5});
+		});
+	});
+	describe('TanhLayer', function(){
+		it('should work', function(){
+			new convnet.TanhLayer({in_sx: 1, in_sy: 1, in_depth: 5});
+		});
+	});
 });
 
 describe('Nets', function(){
@@ -72,6 +122,19 @@ describe('Nets', function(){
 			];
 
 			var net = new convnet.Net(layers);
+		});
+		it('should be able to have a Vol passed through it', function(){
+			var layers = [
+			    {type:'input', out_sx:1, out_sy:1, out_depth:2},
+			    {type:'fc', num_neurons:20, activation:'relu'},
+			    {type:'softmax', num_classes:10}
+			];
+
+			var net = new convnet.Net(layers);
+			var voltype = new convnet.VolType(1, 1, 2);
+			var vol = new voltype([[[1,0]]]);
+
+			net.forward(vol);
 		});
 	});
 });
