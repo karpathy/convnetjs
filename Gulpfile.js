a6e1f2be5240a12bf16fe3e429fd10ed892f28e7
qcgm1978 <zhanghl@strongunion.com.cn>
2016-05-30 18:01:58 +0800

    copy build folder automatically

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,26 +1,19 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
-//var lrserver = require('tiny-lr');
-//var livereload = require('gulp-livereload');
-//var connect = require('connect');
 var browserSync = require('browser-sync');
 const reload = browserSync.reload;
-
-// Edit this values to best suit your app
-var WEB_PORT = 9000;
-var APP_DIR = 'demo';
+gulp.task('copy', () =>
+        gulp.src([
+            'build/**/*'
+        ], {
+            base: './'
+        }).pipe(gulp.dest('demo'))
+);
 // jshint files
 gulp.task('jshint', function () {
     //gulp.src(['test/**/*.js'])
     //    .pipe(jshint())
     //    .pipe(jshint.reporter());
-});
-//var lrs = lrserver();
-// start livereload server
-gulp.task('lr-server', function () {
-    //lrs.listen(35729, function (err) {
-    //    if (err) return console.log(err);
-    //});
 });
 // start local http server for development
 gulp.task('http-server', function () {
@@ -38,25 +31,12 @@ gulp.task('http-server', function () {
         port: 3000
     });
     gulp.watch(['demo/**/*'], reload);
-
-    //connect()
-    //.use(require('connect-livereload')())
-    //.use(connect.static(APP_DIR))
-    //.listen(WEB_PORT);
-    console.log('Server listening on http://localhost:' + WEB_PORT);
 });
 // start local http server with watch and livereload set up
 gulp.task('server', function () {
-    //gulp.run('lr-server');
-    //var watchFiles = ['demo/**/*.html', 'demo/js/**/*.js'];
-    //gulp.watch(watchFiles, function (e) {
-    //    console.log('Files changed. Reloading...');
-    //    gulp.src(watchFiles)
-    //        .pipe(livereload(lrs));
-    //});
     gulp.run('http-server');
 });
 gulp.task('default', function () {
-    gulp.run('jshint', 'server');
+    gulp.run('jshint', 'copy', 'server');
 });
 
